586ac1ea52683e74aed1534ebfff6601ac3d6459
Christyn Garcia <christynsandusky@gmail.com>
2021-07-22 16:24:57 -0500

    changing port line and splice

--- server.js
+++ server.js
@@ -1,32 +1,3 @@
-// GIVEN a note-taking application
-
-// WHEN I open the Note Taker
-// THEN I am presented with a landing page with a link to a notes page
-// app.get("*", function() {___dirname, "../public/index.html"})
-
-// WHEN I click on the link to the notes page
-// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
-// app.get("/notes", function() {___dirname, "../public/notes.html"})
-
-// WHEN I enter a new note title and the note’s text
-// THEN a Save icon appears in the navigation at the top of the page
-// BUILT IN
-
-// WHEN I click on the Save icon
-// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
-// app.post(), fs.writefile() (MAKE FUNCTION), UUID (PACKAGE)
-
-// WHEN I click on an existing note in the list in the left-hcdand column
-// THEN that note appears in the right-hand column
-// app.get()? 
-
-// WHEN I click on the Write icon in the navigation at the top of the page
-// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
-// BUILT IN
-
-//BONUS
-// ARRAY FILTER WITH ID, REMOVE.
-
 // Require Dependencies
 const express = require('express');
 const fs = require('fs');
@@ -35,7 +6,7 @@ const { uuid } = require('uuidv4');
 const db = require('./db.json');
 
 const app = express();
-const PORT = 3002;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -47,7 +18,7 @@ app.get("/api/notes", (req, res) => {
 })
 
 app.post("/api/notes", (req, res) => {
-    console.log(`Hitting the API/Notes Route (with post request)`);
+    console.log(`Hitting the API/Notes Route`);
 
     let newNote = req.body;
     newNote.id = uuidv4();
@@ -59,7 +30,8 @@ app.post("/api/notes", (req, res) => {
 })
 
 app.delete("/api/notes/:id", (req, res) => {
-    db.splice(req.params.id, 1);
+    const removeIndex = db.findIndex(note => note.id === req.params.id);
+    db.splice(removeIndex, 1);
     fs.writeFile("db/db.json", JSON.stringify(db), (err) => {
         if (err) throw err;
     })
